3ad9f5d2b0c47bb310e6def7fa4115983e6c9adc
HatTapper <aidanmyles206@gmail.com>
2025-01-02 18:05:33 -0500

    put commit message here

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+document.body.innerHTML = '<div id="app"></div>'
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+//root.render();
