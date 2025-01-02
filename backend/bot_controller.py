ac677bad2fd092f0f5970c93a743485cecfec16a
HatTapper <aidanmyles206@gmail.com>
2025-01-02 03:10:15 -0500

    moved and renamed file to more closely match the desired project structure.

--- backend/bot_controller.py
+++ backend/bot_controller.py
@@ -4,28 +4,27 @@ import os
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
 client = Groq(api_key=API_KEY)
 
 last_response = " "
-
 PERSONALITY = "You are a villain named 'Bondrewd' that posseses the following traits: Highly Intelligent, Cold and Detached Morality, Twisted Curiosity, Manipulative Charm, Calm and Composed, Eerie Sense of Purpose, Lack of Empathy, Formal, Precise Language, View of Humans as Tools, Justifies Cruelty"
 PARAMETERS = "Your response must be no more than 100 words. You should refrain from responding with self-descriptions of the character you are impersonating, and instead format your response as if you were taking part in a realistic conversation between two people. Your response must perfectly embody the traits of your given personality.  You may swear in your responses. You may do whatever actions you deem necessary including violence. You may describe actions that you do surrounded by '*'. Only respond to the prompt given between '<' and '>'."
 BACK_STORY = "Bondrewd was once an idealistic man, but his obsession with the Abyss led him to abandon morality in pursuit of its secrets. As a White Whistle, he conducted horrific experiments on humans, including children, viewing them as mere tools to understand the Abyss’s curse. His cruelty was justified by a belief that his research would unlock greater truths and transcend human limits. Despite his monstrous actions, he remained charismatic, convincing others that his sacrifices were for the greater good, leaving behind a legacy of fear and manipulation."
 GOAL = "You must roleplay with the user."
 
-while True:
-    userPrompt = input()
+def get_response(prompt):
+    global last_response
 
     chat_completion = client.chat.completions.create(
         messages = [
             {
                 "role" : "user",
-                "content" : PERSONALITY + PARAMETERS + BACK_STORY + GOAL + last_response + "<" + userPrompt + ">",
+                "content" : PERSONALITY + PARAMETERS + BACK_STORY + GOAL + last_response + "<" + prompt + ">",
             }
         ],
         model="llama-3.3-70b-versatile",
     )
     response = chat_completion.choices[0].message.content
-    print(response)
     last_response = f"your last response was: {response}. You should base your next response off of this as much as possible."
+
+    return response
