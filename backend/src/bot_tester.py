8d9f1eab8362b4213bcbd9d1151fc26cc1e6f0e2
HatTapper <aidanmyles206@gmail.com>
2025-01-03 03:47:54 -0500

    added new bot personality "GTA Thug"

--- backend/src/bot_tester.py
+++ backend/src/bot_tester.py
@@ -1,7 +1,7 @@
 from backend.src.bot_controller import get_response_character, choose_character_debug
 from bot_controller import choose_character, CHARACTERS
 
-selected_character = choose_character_debug("Bondrewd")
+selected_character = choose_character_debug("Thug")
 
 while True:
     user_input = input("> ")
