aa7693d891892fd1c7ca87765d4b1583f352cda3
HatTapper <aidanmyles206@gmail.com>
2025-01-03 01:16:42 -0500

    added functionality for changing the character the user is talking to

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,56 +6,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    document.body.style = "background-color: #3F3F4F;"
 
-  // new line start
-  const [profileData, setProfileData] = useState(null)
+    // new line start
+    const [profileData, setProfileData] = useState(null)
 
-  function getData() {
-    axios({
-      method: "GET",
-      url:"/profile",
-    })
-        .then((response) => {
-          const res =response.data
-          setProfileData(({
-            profile_name: res.name,
-            about_me: res.about}))
-        }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-      }
-    })}
+    /*function getData() {
+        axios({
+                  method: "GET",
+                  url:"/profile",
+        })
+                .then((response) => {
+                    const res =response.data
+                    setProfileData(({
+                        profile_name: res.name,
+                        about_me: res.about}))
+                }).catch((error) => {
+                if (error.response) {
+                    console.log(error.response)
+                    console.log(error.response.status)
+                    console.log(error.response.headers)
+                }})}*/
   //end of new line
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+    function selectBondrewd()
+    {
+        axios({
+            method: "GET",
+            url:"/bondrewd",
+        }).then((response) => {
+            console.log(response)
+        })
+    }
 
-          {/* new line start*/}
-          <p>To get your profile details: </p><button onClick={getData}>Click me</button>
-          {profileData && <div>
-            <p>Profile name: {profileData.profile_name}</p>
-            <p>About me: {profileData.about_me}</p>
-          </div>
-          }
-          {/* end of new line */}
-        </header>
-      </div>
-  );
+    function selectAinz()
+    {
+        axios({
+            method: "GET",
+            url:"/ainz",
+        }).then((response) => {
+            console.log(response)
+        })
+    }
+
+    return (
+        <div className="App">
+            <form action="/post" method="post" target="output">
+                <label>
+                    Enter your prompt here.
+                    <input type="text" name="prompt"/>
+                </label>
+                <input type="submit"/>
+            </form>
+
+            <button onClick={selectBondrewd}>Select Bondrewd</button>
+            <button onClick={selectAinz}>Select Ainz</button>
+
+              <iframe name="output" />
+        </div>
+    );
 }
 
 export default App;
